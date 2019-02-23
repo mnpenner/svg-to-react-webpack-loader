901352034698b44f2a6a25e517ed6bf85538707a
Mark Penner <code@mpen.ca>
2019-02-23 14:44:08 -0800

    Published!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# react-svg-loader
+# svg-to-react-webpack-loader
 
-A webpack loader that allows you to import .svg files into a React app and have them inlined without an additional HTTP request.
+A [webpack](https://webpack.js.org/) loader that allows you to import .svg files into a [React](https://reactjs.org/) app and have them inlined without incurring an additional HTTP request.
 
 Also allows you to add props onto the `<svg>` element at run-time, and a `<title>` and `<desc>` for accessibility.
 
@@ -17,13 +17,10 @@ Also allows you to add props onto the `<svg>` element at run-time, and a `<title
 ### Output
 
 ```
-export default function Svg({title, desc, ...props}) {
-    return null || React.createElement("svg", Object.assign({
-        xmlns: "http://www.w3.org/2000/svg",
-        viewbox: "0 0 320 512"
-    }, props), title != null ? React.createElement("title", null, title) : null, desc != null ? React.createElement("desc", null, desc) : null, null || React.createElement("path", {d: "M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"}, null))
-}
-Svg.displayName = "src/images/chevron-right.svg"
+import {createElement as e} from 'react'
+const C=({title,desc,...props})=>e("svg",Object.assign({xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 320 512"},props),title!=null?e("title",null,title):null,desc!=null?e("desc",null,desc):null,e("path",{d:"M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"}))
+C.displayName="src/images/chevron-right.svg"
+export default C
 ```
 
 
@@ -75,3 +72,16 @@ declare module "*.svg" {
     export default content;
 }
 ```
+
+## Prior Art
+
+- [react-svg-loader](https://www.npmjs.com/package/react-svg-loader)
+- [svg-react-loader](https://www.npmjs.com/package/svg-react-loader)
+- [svg-to-react-loader](https://www.npmjs.com/package/svg-to-react-loader)
+- [svg2react-loader](https://www.npmjs.com/package/svg2react-loader)
+
+These names were already registered on npm 😢 No code was borrowed and no comparison is provided.
+
+## License
+
+MIT
--- react-svg-loader.js
+++ react-svg-loader.js
@@ -1,57 +1,64 @@
 const Path = require('path');
 const htmlparser = require('htmlparser2')
-const js = require('js-serialize');
+const toJs = require('js-serialize');
+const stdAttrs = require('./std-attrs');
 
 module.exports = function(content) {
     const loader = this;
     loader.cacheable && loader.cacheable();
     const callback = loader.async();
-    const sb = ['export default function Svg({title,desc,...props}) { return null'];
+    const sb = ["import {createElement as e} from 'react'\n",'const C=({title,desc,...props})=>'];
     let svgOpen = false;
 
     const parser = new htmlparser.Parser({
-        onopentag(name, attrs) {
-            if(name === 'svg') {
-                svgOpen = true;
+        onopentag(tagName, attrs) {
+            const isSvg = tagName.toLowerCase() === 'svg';
+            if(isSvg) svgOpen = true;
+            if(!svgOpen) return;
+            if(attrs.hasOwnProperty('style')) {
+                throw new Error(`Sorry, "style" attributes are not presently supported (requires parsing CSS)`)
             }
-            if(svgOpen) {
-                if(attrs.hasOwnProperty('class')) {
-                    attrs.className = attrs.class;
-                    delete attrs.class;
+            for(let attrName of Object.keys(attrs)) {
+                if(stdAttrs.hasOwnProperty(attrName) && attrName !== stdAttrs[attrName]) {
+                    if(attrs.hasOwnProperty(stdAttrs[attrName])) {
+                        loader.emitWarning(new Error(`<${tagName}> has both "${attrName}" and "${stdAttrs[attrName]}" attributes`));
+                    } else {
+                        attrs[stdAttrs[attrName]] = attrs[attrName];
+                        delete attrs[attrName];
+                    }
                 }
-                if(attrs.hasOwnProperty('style')) {
-                    throw new Error("Sorry, `style` attributes are not presently supported (requires parsing CSS)")
-                }
-                let attrCode = js(attrs);
-                let contentCode = 'null';
-                if(name === 'svg') {
-                    attrCode = `Object.assign(${attrCode},props)`;
-                    contentCode = ['title','desc'].map(a => `${a}!=null?React.createElement(${js(a)},null,${a}):null`).join(',') + `,${contentCode}`
-                }
-                sb.push(`||React.createElement(${js(name)},${attrCode},${contentCode}`);
             }
+
+            if(!isSvg) sb.push(',');
+            sb.push(`e(`,toJs(tagName));
+            let attrCode = toJs(attrs);
+            if(isSvg) attrCode = `Object.assign(${attrCode},props)`;
+            sb.push(',',attrCode);
+            if(isSvg) sb.push(',',['title', 'desc'].map(a => `${a}!=null?e(${toJs(a)},null,${a}):null`).join(','));
         },
         onclosetag(name) {
-            if(svgOpen) {
-                sb.push(')');
-                if(name === 'svg') {
-                    svgOpen = false;
-                }
+            if(!svgOpen) return;
+            sb.push(')');
+            if(name === 'svg') {
+                svgOpen = false;
             }
         },
         ontext(text) {
-            sb.push('||',js(text));
+            text = text.trim();
+            if(text.length) {
+                sb.push(',', toJs(text));
+            }
         },
         onerror(err) {
             callback(err);
         },
         onend() {
-            sb.push(`}Svg.displayName=${js(Path.relative(loader.rootContext,loader.resourcePath))}`)
+            sb.push(`\nC.displayName=${toJs(Path.relative(loader.rootContext, loader.resourcePath))}\nexport default C`)
             const svg = sb.join('');
             // console.log(svg);
             callback(null, svg)
         }
-    }, {decodeEntities: false, recognizeSelfClosing: true, lowerCaseTags: true})
+    }, {decodeEntities: false, recognizeSelfClosing: true, lowerCaseTags: false})
 
     parser.write(content)
     parser.end()
